937023b9703ae20561e5a3e62b0ad2640b3e9480
Vijneder Singh <vijen2000@gmail.com>
2025-07-06 18:31:13 +0530

    NAT and Elastic IP

---
+++ terraform/nat-gateway.tf
@@ -0,0 +1,14 @@
+resource "aws_eip" "nat_IP" {
+    count = length(var.cidr_private_subnets)
+    vpc = true
+}
+
+resource "" "name" {
+  count = length(var.cidr_private_subnets)
+  depends_on = [aws_eip.nat_IP]
+  allocation_id = aws_eip.nat_IP[count.index].id
+  subnet_id = aws_subnet.my_project_private_subnet[count.index].id
+  tags = {
+    "Name" = "Private NAT GW: For private subnet"
+  }
+}
