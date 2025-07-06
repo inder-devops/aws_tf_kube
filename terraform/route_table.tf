ca5add7b8090bf3226f3fe8e21cbfa83806d7f58
Vijneder Singh <vijen2000@gmail.com>
2025-07-06 19:00:09 +0530

    correction

--- terraform/route_table.tf
+++ terraform/route_table.tf
@@ -3,7 +3,7 @@ resource "aws_route_table" "my_public_route_table" {
     vpc_id = aws_vpc.my_project_vpc
     
     route {
-        cidr_block = 0.0.0.0/0
+        cidr_block = "0.0.0.0/0"
         gateway_id = aws_internet_gateway.public_int_gw.id
     }
     
@@ -19,7 +19,7 @@ resource "aws_route_table" "my_private_route_table" {
     count = length(var.cidr_private_subnets)
 
     route = {
-        cidr_block = 0.0.0.0/0
+        cidr_block = "0.0.0.0/0"
         nat_gateway_id = aws_nat_gateway.my_nat_gateway[count.index].id
     }
     
